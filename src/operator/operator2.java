a92ff4adc3c3f6754f92d8b78a61a4ddbac1ef55
brick <94353186+brickbrickmon@users.noreply.github.com>
2025-10-30 12:28:32 +0900

    feat opration2 commit

--- src/operator/operator2.java
+++ src/operator/operator2.java
@@ -2,7 +2,24 @@ package operator;
 
 public class operator2 {
     public static void main(String[] args) {
-        System.out.println("haha");
 
+        // 문자열 + 문자열 1
+        String s1 = "hello";
+        String s2 = "world";
+        System.out.println(s1 + s2);
+
+        // 문자열  + 문자열 2
+        String s3 = "hello" + "world222";
+        System.out.println(s3);
+
+        // 문자열 + 숫자 1 -> 숫자도 문자열로 바뀌어 버림.
+        int s4 = 10;
+        String s5 = "문자열 옆에 숫자가 붙으면 같이 문자열이 되어버리지." + s4;
+        System.out.println(s5);
+
+        // 문자열 + 숫자 2
+        int s6 = 20;
+        String s7 = "숫자가 아닌 변수로 더해도 똑같지." + s6;
+        System.out.println(s7);
     }
 }
