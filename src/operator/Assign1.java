591430e24cbc3cbe10614332a446eb34dbe0893e
brick <94353186+brickbrickmon@users.noreply.github.com>
2025-12-10 04:57:57 +0900

    feat logical commit

---
+++ src/operator/Assign1.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class Assign1 {
+
+    static void main(String[] args) {
+        System.out.println("i = i + a >> i += a");
+        int a = 5; // 5
+        a += 3;    // 8
+        a -= 2;    // 6
+        a *= 4;    // 24
+        a /= 3;    // 8
+        a %= 5;    // 3
+        System.out.println("a = " + a);
+
+    }
+}
---
+++ src/operator/OperationEx3.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperationEx3 {
+
+    static void main(String[] args) {
+
+        int score = 70;
+
+//        System.out.println(80 <= score && score <= 100);
+        boolean result = 80 <= score && score <= 100;
+
+        System.out.println("result = " + result);
+
+    }
+}
